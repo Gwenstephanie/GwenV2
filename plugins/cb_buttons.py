143574598189da93f23b4263934bb764f3fc53c1
Gwenstephanie <53878781+Gwenstephanie@users.noreply.github.com>
2020-05-19 19:04:35 +0100

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -8,11 +8,7 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-import json
-import math
 import os
-import shutil
-import subprocess
 import time
 
 # the secret configuration specific things
@@ -28,107 +24,241 @@ import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
-from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
-from plugins.youtube_dl_button import youtube_dl_call_back
-from plugins.dl_button import ddl_call_back
+from helper_funcs.display_progress import progress_for_pyrogram
+from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot, cult_small_video
+
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-# https://stackoverflow.com/a/37631799/4723940
-from PIL import Image
 
 
-@pyrogram.Client.on_callback_query()
-async def button(bot, update):
+@pyrogram.Client.on_message(pyrogram.Filters.command(["ffmpegrobot"]))
+async def ffmpegrobot_ad(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
-            chat_id=update.message.chat.id,
-            message_ids=update.message.message_id,
+            chat_id=update.chat.id,
+            message_ids=update.message_id,
             revoke=True
         )
         return
-    # logger.info(update)
-    cb_data = update.data
-    if ":" in cb_data:
-        # unzip formats
-        extract_dir_path = Config.DOWNLOAD_LOCATION + \
-            "/" + str(update.from_user.id) + "zipped" + "/"
-        if not os.path.isdir(extract_dir_path):
-            await bot.delete_messages(
-                chat_id=update.message.chat.id,
-                message_ids=update.message.message_id,
-                revoke=True
-            )
-            return False
-        zip_file_contents = os.listdir(extract_dir_path)
-        type_of_extract, index_extractor, undefined_tcartxe = cb_data.split(":")
-        if index_extractor == "NONE":
-            try:
-                shutil.rmtree(extract_dir_path)
-            except:
-                pass
-            await bot.edit_message_text(
-                chat_id=update.message.chat.id,
-                text=Translation.CANCEL_STR,
-                message_id=update.message.message_id
-            )
-        elif index_extractor == "ALL":
-            i = 0
-            for file_content in zip_file_contents:
-                current_file_name = os.path.join(extract_dir_path, file_content)
-                start_time = time.time()
-                await bot.send_document(
-                    chat_id=update.message.chat.id,
-                    document=current_file_name,
-                    # thumb=thumb_image_path,
-                    caption=file_content,
+    TRChatBase(update.from_user.id, update.text, "ffmpegrobot")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.FF_MPEG_RO_BOT_AD_VER_TISE_MENT,
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["trim"]))
+async def trim(bot, update):
+    if update.from_user.id not in Config.AUTH_USERS:
+        await bot.delete_messages(
+            chat_id=update.chat.id,
+            message_ids=update.message_id,
+            revoke=True
+        )
+        return
+    TRChatBase(update.from_user.id, update.text, "trim")
+    saved_file_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".FFMpegRoBot.mkv"
+    if os.path.exists(saved_file_path):
+        a = await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.DOWNLOAD_START,
+            reply_to_message_id=update.message_id
+        )
+        commands = update.command
+        if len(commands) == 3:
+            # output should be video
+            cmd, start_time, end_time = commands
+            o = await cult_small_video(saved_file_path, Config.DOWNLOAD_LOCATION, start_time, end_time)
+            logger.info(o)
+            if o is not None:
+                await bot.edit_message_text(
+                    chat_id=update.chat.id,
+                    text=Translation.UPLOAD_START,
+                    message_id=a.message_id
+                )
+                c_time = time.time()
+                await bot.send_video(
+                    chat_id=update.chat.id,
+                    video=o,
+                    # caption=description,
+                    # duration=duration,
+                    # width=width,
+                    # height=height,
+                    supports_streaming=True,
                     # reply_markup=reply_markup,
-                    reply_to_message_id=update.message.message_id,
+                    # thumb=thumb_image_path,
+                    reply_to_message_id=update.message_id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
-                        update.message,
-                        start_time
+                        a,
+                        c_time
                     )
                 )
-                i = i + 1
-                os.remove(current_file_name)
-            try:
-                shutil.rmtree(extract_dir_path)
-            except:
-                pass
-            await bot.edit_message_text(
-                chat_id=update.message.chat.id,
-                text=Translation.ZIP_UPLOADED_STR.format(i, "0"),
-                message_id=update.message.message_id
-            )
+                os.remove(o)
+                await bot.edit_message_text(
+                    chat_id=update.chat.id,
+                    text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
+                    disable_web_page_preview=True,
+                    message_id=a.message_id
+                )
+        elif len(commands) == 2:
+            # output should be screenshot
+            cmd, start_time = commands
+            o = await take_screen_shot(saved_file_path, Config.DOWNLOAD_LOCATION, start_time)
+            logger.info(o)
+            if o is not None:
+                await bot.edit_message_text(
+                    chat_id=update.chat.id,
+                    text=Translation.UPLOAD_START,
+                    message_id=a.message_id
+                )
+                c_time = time.time()
+                await bot.send_document(
+                    chat_id=update.chat.id,
+                    document=o,
+                    # thumb=thumb_image_path,
+                    # caption=description,
+                    # reply_markup=reply_markup,
+                    reply_to_message_id=update.message_id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        a,
+                        c_time
+                    )
+                )
+                c_time = time.time()
+                await bot.send_photo(
+                    chat_id=update.chat.id,
+                    photo=o,
+                    # caption=Translation.CUSTOM_CAPTION_UL_FILE,
+                    reply_to_message_id=update.message_id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        a,
+                        c_time
+                    )
+                )
+                os.remove(o)
+                await bot.edit_message_text(
+                    chat_id=update.chat.id,
+                    text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
+                    disable_web_page_preview=True,
+                    message_id=a.message_id
+                )
         else:
-            file_content = zip_file_contents[int(index_extractor)]
-            current_file_name = os.path.join(extract_dir_path, file_content)
-            start_time = time.time()
-            await bot.send_document(
-                chat_id=update.message.chat.id,
-                document=current_file_name,
-                # thumb=thumb_image_path,
-                caption=file_content,
-                # reply_markup=reply_markup,
-                reply_to_message_id=update.message.message_id,
+            await bot.edit_message_text(
+                chat_id=update.chat.id,
+                text=Translation.FF_MPEG_RO_BOT_RE_SURRECT_ED,
+                message_id=a.message_id
+            )
+    else:
+        # reply help message
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.FF_MPEG_RO_BOT_STEP_TWO_TO_ONE,
+            reply_to_message_id=update.message_id
+        )
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["storageinfo"]))
+async def storage_info(bot, update):
+    if update.from_user.id not in Config.AUTH_USERS:
+        await bot.delete_messages(
+            chat_id=update.chat.id,
+            message_ids=update.message_id,
+            revoke=True
+        )
+        return
+    TRChatBase(update.from_user.id, update.text, "storageinfo")
+    saved_file_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".FFMpegRoBot.mkv"
+    if os.path.exists(saved_file_path):
+        metadata = extractMetadata(createParser(saved_file_path))
+        duration = None
+        if metadata.has("duration"):
+            duration = metadata.get('duration')
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.FF_MPEG_RO_BOT_STOR_AGE_INFO.format(duration),
+            reply_to_message_id=update.message_id
+        )
+    else:
+        # reply help message
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.FF_MPEG_RO_BOT_STEP_TWO_TO_ONE,
+            reply_to_message_id=update.message_id
+        )
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["clearffmpegmedia"]))
+async def clear_media(bot, update):
+    if update.from_user.id not in Config.AUTH_USERS:
+        await bot.delete_messages(
+            chat_id=update.chat.id,
+            message_ids=update.message_id,
+            revoke=True
+        )
+        return
+    TRChatBase(update.from_user.id, update.text, "clearffmpegmedia")
+    saved_file_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".FFMpegRoBot.mkv"
+    if os.path.exists(saved_file_path):
+        os.remove(saved_file_path)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.FF_MPEG_DEL_ETED_CUSTOM_MEDIA,
+        reply_to_message_id=update.message_id
+    )
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["downloadmedia"]))
+async def download_media(bot, update):
+    if update.from_user.id not in Config.AUTH_USERS:
+        await bot.delete_messages(
+            chat_id=update.chat.id,
+            message_ids=update.message_id,
+            revoke=True
+        )
+        return
+    TRChatBase(update.from_user.id, update.text, "downloadmedia")
+    saved_file_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".FFMpegRoBot.mkv"
+    if not os.path.exists(saved_file_path):
+        a = await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.DOWNLOAD_START,
+            reply_to_message_id=update.message_id
+        )
+        try:
+            c_time = time.time()
+            await bot.download_media(
+                message=update.reply_to_message,
+                file_name=saved_file_path,
                 progress=progress_for_pyrogram,
                 progress_args=(
-                    Translation.UPLOAD_START,
-                    update.message,
-                    start_time
+                    Translation.DOWNLOAD_START,
+                    a,
+                    c_time
                 )
             )
-            try:
-                shutil.rmtree(extract_dir_path)
-            except:
-                pass
+        except (ValueError) as e:
             await bot.edit_message_text(
-                chat_id=update.message.chat.id,
-                text=Translation.ZIP_UPLOADED_STR.format("1", "0"),
-                message_id=update.message.message_id
+                chat_id=update.chat.id,
+                text=str(e),
+                message_id=a.message_id
             )
-    elif "|" in cb_data:
-        await youtube_dl_call_back(bot, update)
-    elif "=" in cb_data:
-        await ddl_call_back(bot, update)
+        else:
+            await bot.edit_message_text(
+                chat_id=update.chat.id,
+                text=Translation.SAVED_RECVD_DOC_FILE,
+                message_id=a.message_id
+            )
+    else:
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS,
+            reply_to_message_id=update.message_id
+        )
