7e405a064101dacf385561eaed0656f0cefbc836
Gwendolyn <53878781+Gwenstephanie@users.noreply.github.com>
2020-07-27 14:07:25 +0100

    Update Renamevideo.py

--- plugins/Renamevideo.py
+++ plugins/Renamevideo.py
@@ -87,43 +87,180 @@ async def rename_doc(bot, update):
                 chat_id=update.chat.id,
                 message_id=a.message_id
                 )
-            logger.info(the_real_download_location)
-            thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-            if not os.path.exists(thumb_image_path):
-                thumb_image_path = None
-            else:
-                width = 0
-                height = 0
-                metadata = extractMetadata(createParser(thumb_image_path))
-                if metadata.has("width"):
-                    width = metadata.get("width")
-                if metadata.has("height"):
-                    height = metadata.get("height")
-                # resize image
-                # ref: https://t.me/PyrogramChat/44663
-                # https://stackoverflow.com/a/21669827/4723940
-                Image.open(thumb_image_path).convert("RGB").save(thumb_image_path)
-                img = Image.open(thumb_image_path)
-                # https://stackoverflow.com/a/37631799/4723940
-                # img.thumbnail((90, 90))
-                img.resize((320, height))
-                img.save(thumb_image_path, "JPEG")
-                # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
-            c_time = time.time()
-            await bot.send_document(
-                chat_id=update.chat.id,
-                document=new_file_name,
-                thumb=thumb_image_path,
-                caption=description,
-                # reply_markup=reply_markup,
-                reply_to_message_id=update.reply_to_message.message_id,
-                progress=progress_for_pyrogram,
-                progress_args=(
-                    Translation.UPLOAD_START,
-                    a, 
-                    c_time
+            if local_file_name.upper().endswith(("MKV", "MP4", "WEBM")):
+            metadata = extractMetadata(createParser(local_file_name))
+            duration = 0
+            if metadata.has("duration"):
+                duration = metadata.get('duration').seconds
+            #
+            width = 0
+            height = 0
+            thumb_image_path = None
+            if os.path.exists(thumbnail_location):
+                thumb_image_path = await copy_file(
+                    thumbnail_location,
+                    os.path.dirname(os.path.abspath(local_file_name))
                 )
-            )
+            else:
+                thumb_image_path = await take_screen_shot(
+                    local_file_name,
+                    os.path.dirname(os.path.abspath(local_file_name)),
+                    (duration / 2)
+                )
+                # get the correct width, height, and duration for videos greater than 10MB
+                if os.path.exists(thumb_image_path):
+                    metadata = extractMetadata(createParser(thumb_image_path))
+                    if metadata.has("width"):
+                        width = metadata.get("width")
+                    if metadata.has("height"):
+                        height = metadata.get("height")
+                    # resize image
+                    # ref: https://t.me/PyrogramChat/44663
+                    # https://stackoverflow.com/a/21669827/4723940
+                    Image.open(thumb_image_path).convert(
+                        "RGB"
+                    ).save(thumb_image_path)
+                    img = Image.open(thumb_image_path)
+                    # https://stackoverflow.com/a/37631799/4723940
+                    img.resize((320, height))
+                    img.save(thumb_image_path, "JPEG")
+                    # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
+            #
+            thumb = None
+            if thumb_image_path is not None and os.path.isfile(thumb_image_path):
+                thumb = thumb_image_path
+            # send video
+            if edit_media and message.photo:
+                sent_message = await message.edit_media(
+                    media=InputMediaVideo(
+                        media=local_file_name,
+                        thumb=thumb,
+                        caption=caption_str,
+                        parse_mode="html",
+                        width=width,
+                        height=height,
+                        duration=duration,
+                        supports_streaming=True
+                    )
+                    # quote=True,
+                )
+            else:
+                sent_message = await message.reply_video(
+                    video=local_file_name,
+                    # quote=True,
+                    caption=caption_str,
+                    parse_mode="html",
+                    duration=duration,
+                    width=width,
+                    height=height,
+                    thumb=thumb,
+                    supports_streaming=True,
+                    disable_notification=True,
+                    # reply_to_message_id=message.reply_to_message.message_id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        "trying to upload",
+                        message_for_progress_display,
+                        start_time
+                    )
+                )
+            if thumb is not None:
+                os.remove(thumb)
+        elif local_file_name.upper().endswith(("MP3", "M4A", "M4B", "FLAC", "WAV")):
+            metadata = extractMetadata(createParser(local_file_name))
+            duration = 0
+            title = ""
+            artist = ""
+            if metadata.has("duration"):
+                duration = metadata.get('duration').seconds
+            if metadata.has("title"):
+                title = metadata.get("title")
+            if metadata.has("artist"):
+                artist = metadata.get("artist")
+            thumb_image_path = None
+            if os.path.isfile(thumbnail_location):
+                thumb_image_path = await copy_file(
+                    thumbnail_location,
+                    os.path.dirname(os.path.abspath(local_file_name))
+                )
+            thumb = None
+            if thumb_image_path is not None and os.path.isfile(thumb_image_path):
+                thumb = thumb_image_path
+            # send audio
+            if edit_media and message.photo:
+                sent_message = await message.edit_media(
+                    media=InputMediaAudio(
+                        media=local_file_name,
+                        thumb=thumb,
+                        caption=caption_str,
+                        parse_mode="html",
+                        duration=duration,
+                        performer=artist,
+                        title=title
+                    )
+                    # quote=True,
+                )
+            else:
+                sent_message = await message.reply_audio(
+                    audio=local_file_name,
+                    # quote=True,
+                    caption=caption_str,
+                    parse_mode="html",
+                    duration=duration,
+                    performer=artist,
+                    title=title,
+                    thumb=thumb,
+                    disable_notification=True,
+                    # reply_to_message_id=message.reply_to_message.message_id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        "trying to upload",
+                        message_for_progress_display,
+                        start_time
+                    )
+                )
+            if thumb is not None:
+                os.remove(thumb)
+        else:
+            thumb_image_path = None
+            if os.path.isfile(thumbnail_location):
+                thumb_image_path = await copy_file(
+                    thumbnail_location,
+                    os.path.dirname(os.path.abspath(local_file_name))
+                )
+            # if a file, don't upload "thumb"
+            # this "diff" is a major derp -_- 😔😭😭
+            thumb = None
+            if thumb_image_path is not None and os.path.isfile(thumb_image_path):
+                thumb = thumb_image_path
+            #
+            # send document
+            if edit_media and message.photo:
+                sent_message = await message.edit_media(
+                    media=InputMediaDocument(
+                        media=local_file_name,
+                        thumb=thumb,
+                        caption=caption_str,
+                        parse_mode="html"
+                    )
+                    # quote=True,
+                )
+            else:
+                sent_message = await message.reply_document(
+                    document=local_file_name,
+                    # quote=True,
+                    thumb=thumb,
+                    caption=caption_str,
+                    parse_mode="html",
+                    disable_notification=True,
+                    # reply_to_message_id=message.reply_to_message.message_id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        "trying to upload",
+                        message_for_progress_display,
+                        start_time
+                    )
+                )
             try:
                 os.remove(new_file_name)
             except:
