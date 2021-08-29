9f38f4db35f62a54138ff9b0b6784ad84307f8e0
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-08-29 08:34:02 +0100

    Update get_external_link.py

--- plugins/get_external_link.py
+++ plugins/get_external_link.py
@@ -26,7 +26,6 @@ from translation import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram
 
 
@@ -39,7 +38,6 @@ async def get_link(bot, update):
             revoke=True
         )
         return
-    TRChatBase(update.from_user.id, update.text, "getlink")
     logger.info(update.from_user)
     if update.reply_to_message is not None:
         reply_message = update.reply_to_message
