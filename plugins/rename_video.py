3bc41acb905d9f79e1f2005c13299b17b6bc06ad
女神 <53878781+RomanGoddess@users.noreply.github.com>
2022-07-20 10:03:22 +0100

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -22,7 +22,7 @@ from translation import Translation
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-from pyrogram import Client, filters
+from pyrogram import Client, Filters
 from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
 from helper_funcs.display_progress import progress_for_pyrogram
 
@@ -32,7 +32,7 @@ from hachoir.parser import createParser
 from PIL import Image
 
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["renamevideo"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["renamevideo"]))
 async def rename_video(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
