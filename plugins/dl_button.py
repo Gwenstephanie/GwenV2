e146201ef4f6e6e05d1eb0d74345411ac2e80738
女神 <53878781+RomanGoddess@users.noreply.github.com>
2022-07-20 08:48:40 +0100

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -97,12 +97,6 @@ async def ddl_call_back(bot, update):
                 update.message.message_id,
                 c_time
             )
-        except asyncio.TimeOutError:
-            await bot.edit_message_text(
-                text=Translation.SLOW_URL_DECED,
-                chat_id=update.message.chat.id,
-                message_id=update.message.message_id
-            )
             return False
     if os.path.exists(download_directory):
         end_one = datetime.now()
